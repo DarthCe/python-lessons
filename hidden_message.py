c172ad316d698d2e0700c04f11ad520e845af4b9
Ce Tidler <ctidler@gci.com>
2020-04-16 17:21:07 -0800

    Changed the encryption key so that I don't have to worry about case sensitivity, also added new checks for each case of encrypted letter

--- hidden_message.py
+++ hidden_message.py
@@ -3,18 +3,18 @@
 
 """
 Encryption Key:
-    A = 1
-    B = 2
-    C = 3
-    D = 4
-    E = 5
-    F = 6
-    G = 7
-    H = 8
-    I = 9
-    J = 0
-    K = Z
-    L = Y
+    A = Z
+    B = Y
+    C = X
+    D = W
+    E = V
+    F = U
+    G = T
+    H = S
+    I = R
+    J = Q
+    K = P
+    L = O
 """
 
 # Get user message
@@ -26,61 +26,101 @@ string_to_build = '' # create an empty string that we'll build with characters f
 for letter in message: # loops over each character in the message    
 
     # encryption key
-    if letter.upper() == "A":
-        letter = "1"
-    elif letter.upper() == "B":
-        letter = "2"
-    elif letter.upper() == "C":
-        letter = "3"
-    elif letter.upper() == "D":
-        letter = "4"
-    elif letter.upper() == "E":
-        letter = "5"
-    elif letter.upper() == "F":
-        letter = "6"
-    elif letter.upper() == "G":
-        letter = "7"
-    elif letter.upper() == "H":
-        letter = "8"
-    elif letter.upper() == "I":
-        letter = "9"
-    elif letter.upper() == "J":
-        letter = "0"
-    elif letter == "K":
+    if letter == "A":
         letter = "Z"
-    elif letter == "k":
+    elif letter == "a":
         letter = "z"
-    elif letter == "L":
+    elif letter == "B":
         letter = "Y"
-    elif letter == "l":
+    elif letter == "b":
         letter = "y"
-    elif letter.upper() == "1":
-        letter = "A"
-    elif letter.upper() == "2":
-        letter = "B"
-    elif letter.upper() == "3":
-        letter = "C"
-    elif letter.upper() == "4":
-        letter = "D"
-    elif letter.upper() == "5":
-        letter = "E"
-    elif letter.upper() == "6":
-        letter = "F"
-    elif letter.upper() == "7":
-        letter = "G"
-    elif letter.upper() == "8":
-        letter = "H"
-    elif letter.upper() == "9":
-        letter = "I"
-    elif letter.upper() == "0":
-        letter = "J"
+    elif letter == "C":
+        letter = "X"
+    elif letter == "c":
+        letter = "x"
+    elif letter == "D":
+        letter = "W"
+    elif letter == "d":
+        letter = "w"
+    elif letter == "E":
+        letter = "V"
+    elif letter == "e":
+        letter = "v"
+    elif letter == "F":
+        letter = "U"
+    elif letter == "f":
+        letter = "u"
+    elif letter == "G":
+        letter = "T"
+    elif letter == "g":
+        letter = "t"
+    elif letter == "H":
+        letter = "S"
+    elif letter == "h":
+        letter = "s"
+    elif letter == "I":
+        letter = "R"
+    elif letter == "i":
+        letter = "r"
+    elif letter == "J":
+        letter = "Q"
+    elif letter == "j":
+        letter = "q"
+    elif letter == "K":
+        letter = "P"
+    elif letter == "k":
+        letter = "p"
+    elif letter == "L":
+        letter = "O"
+    elif letter == "l":
+        letter = "o"
     elif letter == "Z":
-        letter = "K"
-    elif letter == "Y":
-        letter = "L"
+        letter = "A"
     elif letter == "z":
-        letter = "k"
+        letter = "a"
+    elif letter == "Y":
+        letter = "B"
     elif letter == "y":
+        letter = "b"
+    elif letter == "X":
+        letter = "C"
+    elif letter == "x":
+        letter = "c"
+    elif letter == "W":
+        letter = "D"
+    elif letter == "w":
+        letter = "d"
+    elif letter == "V":
+        letter = "E"
+    elif letter == "v":
+        letter = "e"
+    elif letter == "U":
+        letter = "F"
+    elif letter == "u":
+        letter = "f"
+    elif letter == "T":
+        letter = "G"
+    elif letter == "t":
+        letter = "g"
+    elif letter == "S":
+        letter = "H"
+    elif letter == "s":
+        letter = "h"
+    elif letter == "R":
+        letter = "I"
+    elif letter == "r":
+        letter = "i"
+    elif letter == "Q":
+        letter = "J"
+    elif letter == "q":
+        letter = "j"
+    elif letter == "P":
+        letter = "K"
+    elif letter == "p":
+        letter = "k"
+    elif letter == "O":
+        letter = "L"
+    elif letter == "o":
         letter = "l"
 
     string_to_build = string_to_build + letter # replaces the characters, else just adds the character to the string
